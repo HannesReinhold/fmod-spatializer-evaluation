5eae466882fa6561ed7042b553a17ee189b41aa9
Hannes Reinhold <hannes.reinhold2001@gmail.com>
2024-03-04 14:59:59 +0100

    Fix missing oculus plugin + fix overlapping windows

--- Assets/Evaluation App/Scripts/Evaluation/GUI/RotateTowardsCam.cs
+++ Assets/Evaluation App/Scripts/Evaluation/GUI/RotateTowardsCam.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:98cd4bccc1d16f491e293a0f150955c1d374feaab729fee0454ddecdff88ebc6
-size 1156
+oid sha256:ec17973ce44355e68837888319682d538abc1cecc3587b4a8fe7302b45383de3
+size 1234
