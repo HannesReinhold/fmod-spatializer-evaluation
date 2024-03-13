718fdb6be046fbfee658fee0d043ebfc969196b0
Hannes Reinhold <hannes.reinhold2001@gmail.com>
2024-03-13 18:13:24 +0100

    Update direction game tutorial

--- Assets/Evaluation App/Scripts/Evaluation/Direction Guessing/DirectionGuessingTutorial.cs
+++ Assets/Evaluation App/Scripts/Evaluation/Direction Guessing/DirectionGuessingTutorial.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7dab2237dcbc638edf43ad485c283c25ad5ae2fe1f3f7bac672c953c1b48520b
-size 13415
+oid sha256:fa0e35eee7aa31c2f3a9d7322eb940cf556b6602ab938ba2698916cd80a2dc3b
+size 12553
