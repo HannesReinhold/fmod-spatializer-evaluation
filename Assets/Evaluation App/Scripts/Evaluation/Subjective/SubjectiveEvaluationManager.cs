fd731bdb35cf3ddf2ba243bade88e003fd3172a1
Hannes Reinhold <hannes.reinhold2001@gmail.com>
2024-03-14 17:54:12 +0100

    Position speakers

--- Assets/Evaluation App/Scripts/Evaluation/Subjective/SubjectiveEvaluationManager.cs
+++ Assets/Evaluation App/Scripts/Evaluation/Subjective/SubjectiveEvaluationManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a638e7f0b2d982a5f43c9c95de8f8c5f9a22ae3f995252640d237b5e2a8ca2f7
-size 4911
+oid sha256:23cc2c518971a6e02d90414baeb685246fefd322505303a762f3828677250f36
+size 4975
