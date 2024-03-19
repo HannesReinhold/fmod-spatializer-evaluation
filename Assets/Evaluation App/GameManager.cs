5b5f968a26bba3f602967c2bfaae8b691aae2a08
Hannes Reinhold <hannes.reinhold2001@gmail.com>
2024-03-19 20:29:13 +0100

    Add tutorial skip button

--- Assets/Evaluation App/GameManager.cs
+++ Assets/Evaluation App/GameManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:00e7e1e7ee324f05090406bc929197b0e2e537d5302da1f7cb35ef987fdfba4e
-size 6334
+oid sha256:f40999a583199ad584e063e3188ba3d3b3647fd433f4a9df60e62892c4799d5b
+size 6511
