8a950a148e32383993916fae746cf5e7d7bbca8d
Hannes Reinhold <hannes.reinhold2001@gmail.com>
2024-03-27 18:38:05 +0100

    Add steam audio spatializer + improve audio mix

--- Assets/Evaluation App/Scripts/Evaluation/Subjective/Guide.cs
+++ Assets/Evaluation App/Scripts/Evaluation/Subjective/Guide.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Guide : MonoBehaviour
 {
     public WindowManager windowManager;
     public List<GameObject> rooms;
+
+    public List<SpatializerSwitch> spatializerObjects;
+
+    public List<ToggleGroup> toggleGroups;
 
 
 
@@ -26,7 +31,9 @@ public class Guide : MonoBehaviour
     {
         windowManager.NextPage();
         OpenRoom(0);
+        FMODUnity.RuntimeManager.StudioSystem.setParameterByName("RoomScale",0);
     }
+
 
     public void OpenRoom(int index)
     {
@@ -34,6 +41,7 @@ public class Guide : MonoBehaviour
         {
             rooms[i].SetActive(i == index);
         }
+        SetSpatializer(0);
     }
 
     public void CloseRoom(int index)
@@ -45,12 +53,14 @@ public class Guide : MonoBehaviour
     {
         OpenRoom(1);
         windowManager.NextPage();
+        FMODUnity.RuntimeManager.StudioSystem.setParameterByName("RoomScale", 0.3f);
     }
 
     public void OnMediumRoomComplete()
     {
         OpenRoom(2);
         windowManager.NextPage();
+        FMODUnity.RuntimeManager.StudioSystem.setParameterByName("RoomScale", 1);
     }
 
     public void OnLargeRoomComplete()
@@ -58,4 +68,37 @@ public class Guide : MonoBehaviour
         CloseRoom(2);
         windowManager.NextPage();
     }
+
+    public void SetSpatializer(int index)
+    {
+        for(int i=0; i<spatializerObjects.Count; i++)
+        {
+
+            for(int j=0; j<3; j++)
+            {
+                spatializerObjects[i].spatializerObjects[j].SetActive(j == index);
+            }
+
+        }
+    }
+
+    public void OnToggleChange(int index)
+    {
+        int value = 0;
+        for (int i = 0; i < toggleGroups.Count; i++)
+        {
+            if (toggleGroups[i].isActiveAndEnabled)
+            {
+                value = i;
+                break;
+            }
+        }
+        SetSpatializer(index);
+    }
+}
+
+[System.Serializable]
+public struct SpatializerSwitch
+{
+    public List<GameObject> spatializerObjects;
 }
